6105b14558fbfd7d52a4940070507fe5ca33352c
Kauan Mattos <kauan.sfrizo@gmail.com>
2026-09-02 22:19:22 -0300

    feat: Modularização criada no APP. Cabecalho, Conteudo, Rodape

---
+++ my-app/src/components/Conteudo/index.tsx
@@ -0,0 +1,11 @@
+
+export default function Conteudo() {
+    return (
+        <main>
+            <section>
+                <h2>Conteúdo Aleatório</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt nulla id neque accusantium magni porro beatae dolor explicabo fuga ad, nostrum ducimus incidunt molestias ipsam tenetur asperiores, recusandae, inventore voluptate.</p>
+            </section>
+        </main>
+    )
+}
