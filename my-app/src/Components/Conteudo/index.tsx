f64f4c85d987d4d422ba17893c653e0694a857fe
Enzo-N-Freitas <enzo_negrao_freitas@outlook.com>
2026-09-02 22:45:06 -0300

    feat: altera favicon da aplicação

--- my-app/src/Components/Conteudo/index.tsx
+++ my-app/src/Components/Conteudo/index.tsx
@@ -1,18 +1,33 @@
+import imgQuadrado from "../../img/quadrado.png";
+
 export default function Conteudo() {
     return (
         <main>
-            <h2>Conteúdo principal</h2>
+            <section>
+                <h2>Conteúdo aleatório</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. In animi, necessitatibus vel obcaecati rerum eos quae doloremque odio placeat veritatis omnis, assumenda corporis quo sed.
+                </p>
+            </section>
+                <h2>Imagem com referência externa</h2>
+                <figure>
+                    <img src="https://placehold.co/600x400/123456/FFFFFF/png" alt="Imagem ilustrativa do conteúdo" />
+                    <figcaption>
+                        Imagem utilizada para representar o conteúdo da aplicação.
+                    </figcaption>
+                </figure>
+            <section>
 
-            <figure>
-                <img
-                    src="https://placehold.co/600x400/FFFFF0/FFFFFF/png"
-                    alt="Imagem ilustrativa do conteúdo"
-                />
-
-                <figcaption>
-                    Imagem utilizada para representar o conteúdo da aplicação.
-                </figcaption>
-            </figure>
+            </section>
+            <section>
+                <h2>Imagem com referência interna</h2>
+                <figure>
+                    <img src={imgQuadrado} alt="Imagem ilustrativa do conteúdo" />
+                    <figcaption>
+                        Imagem utilizada para representar o conteúdo da aplicação.
+                    </figcaption>
+                </figure>
+            </section>
         </main>
     )
 }
