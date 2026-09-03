d810b04112462c2d3d19006a6ac5d9f332322e21
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-09-02 22:19:06 -0300

    feat : Modularização criada no APP. Cabecalho,Conteudo e Rodape.

---
+++ my-app/src/components/Conteudo/index.tsx
@@ -0,0 +1,11 @@
+
+export default function Conteudo() {
+  return (
+    <main>
+        <section>
+            <h2>Conteúdo aleatório</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam nisi voluptatum accusamus facere quod. Esse tempore beatae hic earum fuga odio sint et ea consectetur a? Vero quis voluptatem consequatur.</p>
+        </section>
+    </main>
+  )
+}
