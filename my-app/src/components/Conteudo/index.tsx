03f7673d4026448f5be1e1651d5c9aa7bda9a68f
borbagustavo <gustavo.borba.27@gmail.com>
2026-09-02 22:46:37 -0300

    feat: exercicios finalizados

--- my-app/src/components/Conteudo/index.tsx
+++ my-app/src/components/Conteudo/index.tsx
@@ -1,19 +1,38 @@
-
+import imgSquad from '../../img/quadrado.png';
 
 export default function Conteudo
-() {
-  return (
-   <main>
-    <h2>Conteúdo principal</h2>
+    () {
+    return (
+        <main>
+            <h2>Conteúdo principal</h2>
+            <section>
+                <figure>
+                    <img src="https://placehold.co/600x400/FccFFF0/oooFFFF/png" alt="Imagem ilustrativa do conteúdo" />
+                    <figcaption>
+                        Imagem utilizada para representar o conteúdo da aplicação.
+                    </figcaption>
+                </figure>
+            </section>
 
-    <figure>
-        <img src="https://placehold.co/600x400/FFFFF0/FFFFFF/png" alt="Imagem ilustrativa" />
-    </figure>
+            <section>
+                <figure>
+                    <img src={imgSquad} alt="Imagem ilustrativa" />
+                    <figcaption>
+                        Imagem utilizada para representar o conteúdo da aplicação.
+                    </figcaption>
+                </figure>
 
-    <figcaption>
-        Imagem utilizada para representar o conteúdo da aplicação.
-    </figcaption>
+            </section>
 
-   </main>
-  )
+            <section>
+                <figure>
+                    <img src="/favicon.png" alt="Imagem ilustrativa do conteúdo" />
+                    <figcaption>
+                        Imagem utilizada para representar o conteúdo da aplicação.
+                    </figcaption>
+                </figure>   
+            </section>
+
+        </main>
+    )
 }
