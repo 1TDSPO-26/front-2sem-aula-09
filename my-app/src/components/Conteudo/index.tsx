75ffb67eb3dc1fa3b61c58883379b75aa11b1ba3
di-zanon <zanon_harnisch@hotmail.com>
2026-09-02 22:19:11 -0300

    feat : Modularização criada no APP. Cabecalho, Conteudo e Rodape.

---
+++ my-app/src/components/Conteudo/index.tsx
@@ -0,0 +1,11 @@
+
+export default function Conteudo() {
+  return (
+    <main>
+        <section>
+            <h2>Conteúdo aleatório</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eaque recusandae ea magni asperiores, in harum illum rerum iste tenetur, officiis ducimus necessitatibus vero maxime reprehenderit iure officia corporis maiores!</p>
+        </section>
+    </main>
+  )
+}
