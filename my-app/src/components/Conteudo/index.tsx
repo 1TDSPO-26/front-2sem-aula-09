50dede45c701b5da2e59ed543ac2e43d4365bad2
MuriloSCruzz <cruzmurilo23@gmail.com>
2026-09-02 22:38:10 -0300

    feat: adiciona mais seções e imagens na página conteúdo

--- my-app/src/components/Conteudo/index.tsx
+++ my-app/src/components/Conteudo/index.tsx
@@ -1,18 +1,50 @@
+import imgQuadrado from "../../img/quadrado.png"
+
 export default function Conteudo() {
     return (
         <main>
-            <h2>Conteúdo principal</h2>
+            <section>
+            <h2>Imagem com referência externa</h2>
 
             <figure>
                 <img
-                    src="https://placehold.co/600x400/FFFFF0/FFFFFF/png"
-                    alt="Imagem ilustrativa do conteúdo"
+                    src="https://placehold.co/600x400/000000/FFFFFF/png"
+                    alt="Imagem ilustrativa do conteúdo" width={200}
                 />
 
                 <figcaption>
                     Imagem utilizada para representar o conteúdo da aplicação.
                 </figcaption>
             </figure>
+            </section>
+            <section>
+            <h2>Imagem com referência interna</h2>
+
+            <figure>
+                <img
+                    src={imgQuadrado}
+                    alt="Imagem ilustrativa do conteúdo" width={200}
+                />
+
+                <figcaption>
+                    Imagem utilizada para representar o conteúdo da aplicação.
+                </figcaption>
+            </figure>
+            </section>
+            <section>
+            <h2>Imagem com referência estática</h2>
+
+            <figure>
+                <img
+                    src="/quadrado-2.png"
+                    alt="Imagem ilustrativa do conteúdo" width={200}
+                />
+
+                <figcaption>
+                    Imagem utilizada para representar o conteúdo da aplicação.
+                </figcaption>
+            </figure>
+            </section>
         </main>
     )
 }
