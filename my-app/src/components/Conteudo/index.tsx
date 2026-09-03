2e67d02d902d27cc59f998281e6534834cb7483e
Mateus-Isaque <mateusisaque2007@gmail.com>
2026-09-02 22:51:32 -0300

    feat: colocando o icone no site

--- my-app/src/components/Conteudo/index.tsx
+++ my-app/src/components/Conteudo/index.tsx
@@ -1,12 +1,55 @@
-import React from 'react'
+import imgQuadrado from "../../img/png-front.png";
 
 export default function Conteudo() {
-  return (
-    <main>
-      <h2>Bem-vindo à minha aplicação React!</h2>
-      <section>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium doloribus veritatis distinctio quam eos nesciunt adipisci nulla. Fuga nesciunt recusandae exercitationem omnis! Minus odit omnis itaque atque inventore, pariatur ducimus?</p>
-      </section>
-    </main>
-  )
+    return (
+        <main>
+            <h2>Bem-vindo à minha aplicação React!</h2>
+            <section>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium doloribus veritatis distinctio quam eos nesciunt adipisci nulla. Fuga nesciunt recusandae exercitationem omnis! Minus odit omnis itaque atque inventore, pariatur ducimus?</p>
+            </section>
+
+            <section>
+
+
+                <figure>
+                    <img
+                        src="https://placehold.co/600x400/FF0000/FFFFFF/png"
+                        alt="Imagem ilustrativa do conteúdo" width={200}
+                    />
+
+                    <figcaption>
+                        Imagem utilizada para representar o conteúdo da aplicação.
+                    </figcaption>
+                </figure>
+            </section>
+
+            <section>
+                <h2>Imagem de referência externa</h2>
+                <figure>
+                    <img
+                        src={imgQuadrado}
+                        alt="Imagem ilustrativa do conteúdo" width={200}
+                    />
+
+                    <figcaption>
+                        Imagem utilizada para representar o conteúdo da aplicação.
+                    </figcaption>
+                </figure>
+            </section>
+
+            <section>
+                <h2>Imagem de referência estática</h2>
+                <figure>
+                    <img
+                        src="/quadrado-2.png"
+                        alt="Imagem ilustrativa do conteúdo" width={200}
+                    />
+
+                    <figcaption>
+                        Imagem utilizada para representar o conteúdo da aplicação.
+                    </figcaption>
+                </figure>
+            </section>
+        </main>
+    )
 }
