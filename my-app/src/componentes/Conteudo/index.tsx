789798f5ef1735f2a7db9af4d0b0e57af62baf23
felipeads12 <lipepassos180208gmail.com>
2026-09-02 22:50:10 -0300

    feat : Exercicio feito!

--- my-app/src/componentes/Conteudo/index.tsx
+++ my-app/src/componentes/Conteudo/index.tsx
@@ -1,8 +1,32 @@
 export default function Conteudo() {
-  return (
-    <main>
-      <h2>Conteúdo Aleatorio</h2>
-      <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur officiis odio at ratione natus voluptatibus nisi velit deleniti repellendus inventore, assumenda culpa sunt accusantium perferendis, esse beatae exercitationem quaerat maxime!</h3>
-    </main>
-  )
+    return (
+        <main>
+            <section>
+                <h2>Conteúdo Aleatorio</h2>
+                <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur officiis odio at ratione natus voluptatibus nisi velit    deleniti repellendus inventore, assumenda culpa sunt accusantium perferendis, esse beatae exercitationem quaerat maxime!</h3>
+            </section>
+            <section>
+                <h2>Imagem com referencia externa</h2>
+                <figure>
+                    <img
+                        src="https://placehold.co/600x400/FF0000/FFFFFF/png"
+                        alt="Imagem ilustrativa do conteúdo" />
+                    <figcaption>
+                        Imagem utilizada para representar o conteúdo da aplicação.
+                    </figcaption>
+                </figure>
+            </section>
+            <section>
+                <h2>Imagem com referencia interna</h2>
+                <figure>
+                    <img
+                        src={imgQuadrado}
+                        alt="Imagem ilustrativa do conteúdo" />
+                    <figcaption>
+                        Imagem utilizada para representar o conteúdo da aplicação.
+                    </figcaption>
+                </figure>
+            </section>
+        </main>
+    )
 }
