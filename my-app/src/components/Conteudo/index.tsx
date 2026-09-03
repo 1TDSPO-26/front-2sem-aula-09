d954657c7ad1941bd4c4f54027f3e27693774ef3
borbagustavo <gustavo.borba.27@gmail.com>
2026-09-02 21:55:33 -0300

    feat: adiciona componentes e favicon

---
+++ my-app/src/components/Conteudo/index.tsx
@@ -0,0 +1,19 @@
+
+
+export default function Conteudo
+() {
+  return (
+   <main>
+    <h2>Conteúdo principal</h2>
+
+    <figure>
+        <img src="https://placehold.co/600x400/FFFFF0/FFFFFF/png" alt="Imagem ilustrativa" />
+    </figure>
+
+    <figcaption>
+        Imagem utilizada para representar o conteúdo da aplicação.
+    </figcaption>
+
+   </main>
+  )
+}
---
+++ my-app/src/components/Rodape/index.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Rodape
+() {
+  return (
+    <div>
+
+    </div>
+  )
+}
