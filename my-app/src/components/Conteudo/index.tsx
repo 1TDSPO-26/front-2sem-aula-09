325276b4ce9a391389ff645a9caa1ec41f732803
arthurmartinss <arthurmartinsh6@gmail.com>
2026-09-02 22:47:52 -0300

    feat: manipulação de imagens com referencias  interna , externa , estatica e favicon adicionado

--- my-app/src/components/Conteudo/index.tsx
+++ my-app/src/components/Conteudo/index.tsx
@@ -1,12 +1,56 @@
+import imgQuadrado from "../../img/quadrado.png";
+
 
 export default function Conteudo() {
   return (
     <main>
-        <section>
-            <h1>Conteudo Aleatorio</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas et natus sit delectus porro, deserunt error alias ad. Assumenda, officiis minus ducimus, possimus quam quasi earum nemo quaerat reiciendis nisi enim, veniam vel eveniet facere officia inventore dicta quis. Sint saepe a nostrum repellendus illo est inventore eos quo nulla. Ab quas neque cumque facilis in. Sequi ratione iure dolore cum eos at quod esse velit possimus recusandae dignissimos blanditiis, maiores voluptatibus facilis minima cupiditate ducimus ea quam doloremque doloribus temporibus deleniti necessitatibus! Soluta id provident vero inventore aut numquam error. Hic minus delectus soluta ipsa reprehenderit blanditiis aspernatur veritatis?</p>
-        </section>
-    </main>
-  )
-}
+      <section>
+        <h1>Conteudo Aleatorio</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas et
+          natus sit delectus porro, deserunt error alias ad. Assumenda, officiis
+          minus ducimus, possimus quam quasi earum nemo quaerat reiciendis nisi
+          enim, veniam vel eveniet facere officia inventore dicta quis. Sint
+          saepe a nostrum repellendus illo est inventore eos quo nulla. Ab quas
+          neque cumque facilis in. Sequi ratione iure dolore cum eos at quod
+         
+        </p>
+      </section>
+      <section>
+        <h2>Imagem com referencia externa</h2>
+        <figure>
+          <img src="https://placehold.co/600x400/FF0000/FFFFFF/png"
+            alt="Imagem ilustrativa do conteúdo"/>
 
+          <figcaption>
+            Imagem utilizada para representar o conteúdo da aplicação.
+          </figcaption>
+        </figure>
+      </section>
+      <section>
+        <h2>Imagem com referencia interna</h2>
+        <figure>
+          <img src={imgQuadrado}
+            alt="Imagem ilustrativa do conteúdo"/>
+
+          <figcaption>
+            Imagem utilizada para representar o conteúdo da aplicação.
+          </figcaption>
+        </figure>
+      </section>
+      <section>
+        <h2>Imagem com referencia estatica</h2>
+        <figure>
+          <img src="/quadrado-2.png"
+            alt="Imagem ilustrativa do conteúdo"/>
+
+          <figcaption>
+            Imagem utilizada para representar o conteúdo da aplicação.
+          </figcaption>
+        </figure>
+      </section>
+      
+
+    </main>
+  );
+}
