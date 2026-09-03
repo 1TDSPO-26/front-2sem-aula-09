578fbd70d61bfdbe12504a538ea3ff2d16bd9ff7
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-09-02 22:39:25 -0300

    feat : Trabalhando com imagens, externas, internas e estáticas.

--- my-app/src/components/Conteudo/index.tsx
+++ my-app/src/components/Conteudo/index.tsx
@@ -1,11 +1,43 @@
+import imgQuadrado from "../../img/quadrado.png";
 
 export default function Conteudo() {
   return (
     <main>
-        <section>
-            <h2>Conteúdo aleatório</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam nisi voluptatum accusamus facere quod. Esse tempore beatae hic earum fuga odio sint et ea consectetur a? Vero quis voluptatem consequatur.</p>
-        </section>
+      <section>
+        <h2>Conteúdo aleatório</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam nisi voluptatum accusamus facere quod. Esse tempore beatae hic earum fuga odio sint et ea consectetur a? Vero quis voluptatem consequatur.</p>
+      </section>
+
+      <section>
+        <h2>Imagem com referência externa</h2>
+        <figure>
+          <img src="https://placehold.co/600x400/FF0000/FFFFFF/png" alt="Imagem ilustrativa do conteúdo" width={200} />
+          <figcaption>
+            Imagem utilizada para representar o conteúdo da aplicação.
+          </figcaption>
+        </figure>
+      </section>
+
+      <section>
+        <h2>Imagem com referência interna</h2>
+        <figure>
+          <img src={imgQuadrado} alt="Imagem ilustrativa do conteúdo" width={200}/>
+          <figcaption>
+            Imagem utilizada para representar o conteúdo da aplicação.
+          </figcaption>
+        </figure>
+      </section>
+
+      <section>
+        <h2>Imagem com referência estática</h2>
+        <figure>
+          <img src="/quadrado-2.png" alt="Imagem ilustrativa do conteúdo" width={200} />
+          <figcaption>
+            Imagem utilizada para representar o conteúdo da aplicação.
+          </figcaption>
+        </figure>
+      </section>
+
     </main>
   )
 }
