3e664417c5768a5d1970db3684724d19e78debfa
alexandreteixeiras <teixeiradossantosalexandre@gmail.com>
2026-09-02 22:18:56 -0300

    feat: Modularização criada no APP. Cabecalho, Conteudo e Rodape

---
+++ my-app/src/components/Conteudo/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Conteudo() {
+  return (
+    <main>
+        <section>
+            <h2>Conteudo aleatorio</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis soluta similique eum natus, voluptate quis exercitationem aperiam! Corrupti, voluptates necessitatibus.</p>
+        </section>
+    </main>
+  )
+}
---
+++ my-app/src/components/Rodape/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Rodape() {
+  return (
+    <footer>
+        <p>&copy; 2026 - Todos os direitos reservados</p>
+    </footer>
+  )
+}
