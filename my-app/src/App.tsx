042f3deaecb445e6283a78ef4f0c2f215c7d8cd2
cesarledres <cesarledrescv@gmail.com>
2026-09-02 21:47:14 -0300

    feat: cria estrutura inicial da aplicação

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+
+export default function App() {
+  return (
+    <main>
+      <h1>Minha aplicação React</h1>
+    </main>
+  )
+}
