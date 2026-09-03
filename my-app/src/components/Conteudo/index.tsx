6297e0eb8938deb46580367324062d7af745b4f4
AnaMendes-25 <anaclaramendes2501@gmail.com>
2026-09-02 21:52:59 -0300

    feat: adiciona componentes e favicon

---
+++ my-app/src/components/Conteudo/index.tsx
@@ -0,0 +1,18 @@
+export default function Conteudo() {
+  return (
+    <main>
+      <h2>Conteúdo principal</h2>
+
+      <figure>
+        <img
+          src="https://placehold.co/600x400/FFFFF0/FFFFFF/png"
+          alt="Imagem ilustrativa do conteúdo"
+        />
+
+        <figcaption>
+          Imagem utilizada para representar o conteúdo da aplicação.
+        </figcaption>
+      </figure>
+    </main>
+  )
+}
