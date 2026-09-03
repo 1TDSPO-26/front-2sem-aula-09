256a1613b202ecc95fca34aee4ab9607238f2a68
Enzo-N-Freitas <enzo_negrao_freitas@outlook.com>
2026-09-02 22:21:41 -0300

    feat: adiciona componentes e favicon

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,13 @@
+import Cabecalho from "./Components/Cabecalho";
+import Conteudo from "./Components/Conteudo";
+import Rodape from "./Components/Rodape";
+
 export default function App() {
   return (
-    <main>
-      <h1>Minha aplicação React</h1>
-    </main>
+    <div>
+      <Cabecalho />
+      <Conteudo />
+      <Rodape />
+    </div>
   )
 }
---
+++ my-app/src/Components/Rodape/index.tsx
@@ -0,0 +1,7 @@
+export default function Rodape() {
+    return (
+        <footer>
+            <p>&copy 2026 Minha Aplicação. Todos os direitos reservados.</p>
+        </footer>
+    )
+}
