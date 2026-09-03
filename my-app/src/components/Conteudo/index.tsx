f8bd4cd6b980da7614deeeca487e365e1d60be36
Canevari2 <gabrielpcanevari@gmail.com]>
2026-09-02 22:19:07 -0300

    feat: Modularização criada do APP. Cabecalho, Conteudo e Rodape.

---
+++ my-app/src/components/Conteudo/index.tsx
@@ -0,0 +1,8 @@
+export default function Conteudo() {
+    return (
+        <header>
+            <h1>Conteúdo Aleatório</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque, similique magnam vel alias natus qui quibusdam? Molestiae architecto quam labore minima minus voluptate nihil facere eaque, praesentium, blanditiis nisi mollitia nostrum placeat beatae omnis aliquid cum unde repellendus deleniti quia recusandae est quas? Fuga facere illo rerum et culpa nemo.</p>
+        </header>
+    )
+}
---
+++ my-app/src/components/Rodape/index.tsx
@@ -0,0 +1,7 @@
+export default function Rodape() {
+    return (
+        <footer>
+            <p>&copy; 2026 - Todos os conteudos reservados.</p>
+        </footer>
+    )
+}
